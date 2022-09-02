e969894ed42efd2f732c82ace2ba96e64d272ba7
Breno Battaglin <breno.battaglin@icloud.com>
2022-09-02 14:19:06 -0300

    WIP Create lotus widget

---
+++ lib/breathe/widgets/lotus.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LotusWidget extends StatefulWidget {
+  const LotusWidget({Key? key}) : super(key: key);
+
+  @override
+  State<LotusWidget> createState() => _LotusWidgetState();
+}
+
+class _LotusWidgetState extends State<LotusWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
