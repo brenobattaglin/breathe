b036cb70ff6d3c4e620f0270f8ce070a9c9b743b
Breno Battaglin <breno.battaglin@icloud.com>
2022-09-03 13:09:04 -0300

    Create lotus animation

--- lib/breathe/widgets/lotus.dart
+++ lib/breathe/widgets/lotus.dart
@@ -8,42 +8,75 @@ class LotusWidget extends StatefulWidget {
   State<LotusWidget> createState() => _LotusWidgetState();
 }
 
-class _LotusWidgetState extends State<LotusWidget> {
+class _LotusWidgetState extends State<LotusWidget> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
+    _animationController.repeat(reverse: true);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: _painter(),
+    return Center(
+      child: SizedBox(
+        height: 300,
+        width: 300,
+        child: CustomPaint(
+          painter: _Painter(
+            CurvedAnimation(
+              parent: _animationController,
+              curve: Curves.easeOutQuart,
+              reverseCurve: Curves.easeOutQuart,
+            ),
+          ),
+          size: Size.infinite,
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
 
-class _painter extends CustomPainter {
-  final Animation<double> animation;
-  final Color color;
-  final int count = 6;
-  final Paint circlePaint = Paint()
-    ..color = Colors.purple
-    ..blendMode = BlendMode.modulate;
+class _Painter extends CustomPainter {
+  final Animation<double> _animation;
+  final int _leaves = 10;
+  late final Color _color;
+  late final Paint _circlePaint;
 
-  _painter(
-    this.animation,
-    this.color,
-  ) : super(repaint: animation);
+  _Painter(
+    this._animation,
+  ) : super(repaint: _animation) {
+    _color = const Color(0xFF8FBCBB);
+    _circlePaint = Paint();
+    _circlePaint.color = _color;
+    _circlePaint.blendMode = BlendMode.softLight;
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
     final center = size.center(Offset.zero);
-    final radius = (size.shortestSide * 0.25) * animation.value;
-    for (var i = 0; i < count; i++) {
-      final indexAngle = (i * pi / count * 2);
-      final angle = indexAngle + (pi * 1.5 * animation.value);
+    final radius = (size.shortestSide * 0.25) * _animation.value;
+    for (var i = 0; i < _leaves; i++) {
+      final indexAngle = (i * pi / _leaves * 2);
+      final angle = indexAngle + (pi * 1.5 * _animation.value);
       final offset = Offset(sin(angle), cos(angle)) * radius * 0.985;
-      canvas.drawCircle(center + (offset * animation.value), radius, circlePaint);
+      canvas.drawCircle(center + (offset * _animation.value), radius, _circlePaint);
     }
   }
 
   @override
-  bool shouldRepaint(_painter oldDelegate) {
-    return animation != oldDelegate.animation;
+  bool shouldRepaint(_Painter oldDelegate) {
+    return _animation != oldDelegate._animation;
   }
 }
